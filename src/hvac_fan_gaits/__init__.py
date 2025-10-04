3bb5464f0f9f7565b6ed08f17fe8b52e7dc84205
Aiden Wang <altoauto.tech@gmail.com>
2025-10-04 10:17:05 -0500

    Delete src/hvac_fan_gaits directory

--- src/hvac_fan_gaits/__init__.py
+++
@@ -1,6 +0,0 @@
-"""HVAC Fan Efficiency (Horse Gait Analogy)
-
-Lightweight toolkit to compare fixed-speed, mode-switch, and ideal-variable fan control
-under varying system resistance (clean ↔ fouled filters).
-"""
-__all__ = ["params", "fan", "system", "plots", "main"]
